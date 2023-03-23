26d82d1af92337c2dc10e3ec505e383bd903d64f
louisdefromont <88596228+louisdefromont@users.noreply.github.com>
2023-03-22 20:18:59 -0400

    Make laser guide flush with walls

--- OneInTheChamber/Assets/Scripts/Player/LaserGuide.cs
+++ OneInTheChamber/Assets/Scripts/Player/LaserGuide.cs
@@ -31,12 +31,12 @@ public class LaserGuide : MonoBehaviour
         int bounceCount = 1;
         while(remainingDistance > 0 && bounceCount <= maxBounces)
         {
-            Vector2 widthOffset = bounceDirection.normalized * rayCastWidth * 0.5f;
+            Vector2 forwardWidthOffset = bounceDirection.normalized * rayCastWidth * 0.5f;
             //Rotate width offset 90 degrees clockwise
-            widthOffset = new Vector2(widthOffset.y, -widthOffset.x);
+            Vector2 sideWidthOffset = new Vector2(forwardWidthOffset.y, -forwardWidthOffset.x);
 
-            RaycastHit2D hit1 = Physics2D.Raycast(previousBouncePosition + widthOffset, bounceDirection, remainingDistance, LayerMask.GetMask("Ground"));
-            RaycastHit2D hit2 = Physics2D.Raycast(previousBouncePosition - widthOffset, bounceDirection, remainingDistance, LayerMask.GetMask("Ground"));
+            RaycastHit2D hit1 = Physics2D.Raycast(previousBouncePosition + sideWidthOffset + forwardWidthOffset, bounceDirection, remainingDistance, LayerMask.GetMask("Ground"));
+            RaycastHit2D hit2 = Physics2D.Raycast(previousBouncePosition - sideWidthOffset + forwardWidthOffset, bounceDirection, remainingDistance, LayerMask.GetMask("Ground"));
             RaycastHit2D hit;
 
             // Determine which raycast hit first
@@ -45,29 +45,31 @@ public class LaserGuide : MonoBehaviour
                 if (hit1.distance < hit2.distance)
                 {
                     hit = hit1;
-                    hit.point -= widthOffset;
+                    hit.point -= sideWidthOffset;
                 }
                 else
                 {
                     hit = hit2;
-                    hit.point += widthOffset;
+                    hit.point += sideWidthOffset;
                 }
             }
             else if (hit1.collider != null)
             {
                 hit = hit1;
-                hit.point -= widthOffset;
+                hit.point -= sideWidthOffset;
             }
             else if (hit2.collider != null)
             {
                 hit = hit2;
-                hit.point += widthOffset;
+                hit.point += sideWidthOffset;
             }
             else
             {
                 hit = hit1;
-                hit.point -= widthOffset;
+                hit.point -= sideWidthOffset;
             }
+            // Push the hit point to the edge of the radius
+            hit.point += forwardWidthOffset;
 
             if (hit.collider != null)
             {
