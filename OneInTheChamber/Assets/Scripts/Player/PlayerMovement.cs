5df3cbd529bd90e41832a4fdc454aa9d63db1d0d
louisdefromont <88596228+louisdefromont@users.noreply.github.com>
2023-03-24 23:37:40 -0400

    thing

--- OneInTheChamber/Assets/Scripts/Player/PlayerMovement.cs
+++ OneInTheChamber/Assets/Scripts/Player/PlayerMovement.cs
@@ -180,16 +180,24 @@ public class PlayerMovement : MonoBehaviour
 
             //Add Recoil
             Vector2 newVelocity = rbody.velocity + (-bulletDirection * bulletForce);
-            rbody.velocity = new Vector2(Mathf.Clamp(newVelocity.x, -trueMaxSpeed, trueMaxSpeed), Mathf.Clamp(newVelocity.y, -trueMaxSpeed, trueMaxSpeed));
-            float dirChange = 1.5f;
-            if (rbody.velocity.magnitude < dirChange)
-            {
-                rbody.velocity = bulletDirection.normalized * dirChange;
-            }
-            // Alternate way to clamp speed
-            //rbody.velocity = Vector2.ClampMagnitude(newVelocity, trueMaxSpeed);
 
-            longJump = false;
+            if ((bulletDirection.x > 0 && newVelocity.x > 0) || (bulletDirection.x < 0 && newVelocity.x < 0))
+            {
+                rbody.velocity = new Vector2(Mathf.Clamp(-newVelocity.x, -trueMaxSpeed, trueMaxSpeed), Mathf.Clamp(newVelocity.y, -trueMaxSpeed, trueMaxSpeed));
+            }
+            else if ((bulletDirection.y > 0 && newVelocity.y > 0) || (bulletDirection.y < 0 && newVelocity.y < 0))
+            {
+                rbody.velocity = new Vector2(Mathf.Clamp(newVelocity.x, -trueMaxSpeed, trueMaxSpeed), Mathf.Clamp(-newVelocity.y, -trueMaxSpeed, trueMaxSpeed));
+            }
+            else
+            {
+                rbody.velocity = new Vector2(Mathf.Clamp(newVelocity.x, -trueMaxSpeed, trueMaxSpeed), Mathf.Clamp(newVelocity.y, -trueMaxSpeed, trueMaxSpeed));
+            }
+
+                // Alternate way to clamp speed
+                //rbody.velocity = Vector2.ClampMagnitude(newVelocity, trueMaxSpeed);
+
+                longJump = false;
             if (bulletDirection.y > 0) 
             {
                 coyoteTimer = 0;
