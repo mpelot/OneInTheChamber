f917ed67c30e875323374283b8477619b4510082
Jasmine Karthikeyan <25208576+jaskarth@users.noreply.github.com>
2023-04-03 00:19:14 -0400

    Oops, wrong update method

--- OneInTheChamber/Assets/Scripts/Platform/MovingPlatform.cs
+++ OneInTheChamber/Assets/Scripts/Platform/MovingPlatform.cs
@@ -18,8 +18,7 @@ public class MovingPlatform : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 		// Update lerp amt
         if (dir) {
