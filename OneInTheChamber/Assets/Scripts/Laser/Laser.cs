e90ac2dfee9e423870a65b34e14589951af79e19
BoatK <jmartinobst@gmail.com>
2023-04-13 23:08:48 -0400

    Added Laser Obstacle

---
+++ OneInTheChamber/Assets/Scripts/Laser/Laser.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public enum Direction {Left, Right, Up, Down};
+    public Direction currentDirection = Direction.Left;
+    public float cooldownTime = 1f;
+    public float shootTime = 1f;
+    public GameObject laserBeam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnLasers());
+    }
+
+    private IEnumerator SpawnLasers()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(cooldownTime);
+            GameObject currentBeam = Instantiate(laserBeam, transform);
+            yield return new WaitForSeconds(shootTime);
+        }
+    }
+
+}
---
+++ OneInTheChamber/Assets/Scripts/Laser/LaserBeam.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeam : MonoBehaviour
+{
+    public enum Direction { Left, Right, Up, Down };
+    public Direction currentDirection = Direction.Left;
+    private Vector2 dirVector;
+    private LineRenderer lrender;
+    private BoxCollider2D bCollide;
+    private LayerMask lMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        lrender = GetComponent<LineRenderer>();
+        bCollide = GetComponent<BoxCollider2D>();
+        currentDirection = (Direction)transform.parent.gameObject.GetComponent<Laser>().currentDirection;
+        switch(currentDirection)
+        {
+            case Direction.Left:
+                dirVector = Vector2.left;
+                break;
+            case Direction.Right:
+                dirVector = Vector2.right;
+                break;
+            case Direction.Up:
+                dirVector = Vector2.up;
+                break;
+            case Direction.Down:
+                dirVector = Vector2.down;
+                break;
+        }
+        lMask = LayerMask.GetMask("Ground");
+        StartCoroutine(KillTime());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + (Vector3)(dirVector), dirVector, 100, lMask);
+        if(hit.collider != null)
+        {
+            //If Horizontal
+            if(currentDirection == Direction.Left || currentDirection == Direction.Right)
+            {
+                float distance = hit.point.x - transform.position.x;
+                lrender.SetPosition(1, new Vector3(distance, 0, 0));
+                bCollide.size = new Vector2(Mathf.Abs(distance), bCollide.size.y);
+                bCollide.offset = new Vector2(distance / 2, 0);
+            }
+            //If Vertical
+            else
+            {
+                float distance = hit.point.y - transform.position.y;
+                lrender.SetPosition(1, new Vector3(0, distance, 0));
+                bCollide.size = new Vector2(bCollide.size.x, Mathf.Abs(distance));
+                bCollide.offset = new Vector2(0, distance / 2);
+            }
+        }
+    }
+
+    private IEnumerator KillTime()
+    {
+        yield return new WaitForSeconds(transform.parent.gameObject.GetComponent<Laser>().shootTime);
+        Destroy(gameObject);
+    }
+}
