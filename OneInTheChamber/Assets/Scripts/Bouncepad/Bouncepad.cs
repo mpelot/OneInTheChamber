1dfc6a6735209b0d62f84a3406e256827a7762b7
Jasmine Karthikeyan <25208576+jaskarth@users.noreply.github.com>
2023-04-19 17:28:07 -0400

    Bouncepads

---
+++ OneInTheChamber/Assets/Scripts/Bouncepad/Bouncepad.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncepad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+			Rigidbody2D rbody = collision.gameObject.GetComponent<Rigidbody2D>();
+			rbody.velocity = new Vector2(rbody.velocity.x, 20.5f);
+        }
+    }
+}
