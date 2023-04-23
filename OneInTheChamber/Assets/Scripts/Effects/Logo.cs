463acdbf0a00efc67c19e06bf0f8021e12f763c1
louisdefromont <88596228+louisdefromont@users.noreply.github.com>
2023-04-23 14:47:55 -0400

    Fixed Title Screen

--- OneInTheChamber/Assets/Scripts/Effects/Logo.cs
+++ OneInTheChamber/Assets/Scripts/Effects/Logo.cs
@@ -4,25 +4,21 @@ using UnityEngine;
 
 public class Logo : MonoBehaviour
 {
-    private float timer = 0;
-    private Vector3 start;
-    private Rigidbody2D rbody;
+    public float displacement;
+    public float cycleTime;
+
+    private RectTransform rectTransform;
+    private float yStart;
     // Start is called before the first frame update
     void Start()
     {
-        start = transform.position;
-        rbody = GetComponent<Rigidbody2D>();
+        rectTransform = GetComponent<RectTransform>();
+        yStart = rectTransform.position.y;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    private void Update()
     {
-        timer += .75f * Time.fixedDeltaTime;
-        if (timer >= Mathf.PI * 2)
-        {
-            timer = 0;
-            transform.position = start;
-        }
-        rbody.velocity = new Vector2(0, .15f*Mathf.Cos(timer));
+        rectTransform.position = new Vector3(rectTransform.position.x, yStart + Mathf.Sin(Time.time * (2 * Mathf.PI) / cycleTime) * displacement, rectTransform.position.z);
     }
 }
