443cf618859bdfadfa32f495405a1968cc239b27
mpelot <mwp81301@uga.edu>
2023-04-26 00:46:09 -0400

    Dynamic Cursor

--- OneInTheChamber/Assets/Crosshair.cs
+++ OneInTheChamber/Assets/Crosshair.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Crosshair : MonoBehaviour
 {
     private Camera cam;
+    public Transform playerTransform;
+    private Animator animator;
+    private bool aiming = false;
 
     void Start()
     {
         cam = Camera.main;
         Cursor.visible = false;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,5 +22,24 @@ public class Crosshair : MonoBehaviour
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 10;
         transform.position = cam.ScreenToWorldPoint(mousePos);
+
+        if (Input.GetMouseButtonDown(1) && !aiming)
+        {
+            aiming = true;
+            animator.Play("Crosshair");
+        }
+        if (!aiming)
+        {
+            Vector2 dir = playerTransform.position - transform.position;
+            dir = Mathf.Abs(dir.x) > Mathf.Abs(dir.y) ? Vector2.right * Mathf.Sign(dir.x) : Vector2.up * Mathf.Sign(dir.y);
+            if (dir.x == 1)
+                animator.Play("Right");
+            else if (dir.x == -1)
+                animator.Play("Left");
+            else if (dir.y == 1)
+                animator.Play("Up");
+            else
+                animator.Play("Down");
+        }
     }
 }
