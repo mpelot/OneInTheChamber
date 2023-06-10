7064590021b17077393785a7f75f6fd211b46663
louisdefromont <88596228+louisdefromont@users.noreply.github.com>
2023-06-09 21:44:18 -0400

    queueClosePlatform + Dropper Width

--- OneInTheChamber/Assets/Scripts/Dropper.cs
+++ OneInTheChamber/Assets/Scripts/Dropper.cs
@@ -19,12 +19,13 @@ public class Dropper : MonoBehaviour
 
     private SpriteRenderer spriteRenderer;
     private Orientation orientation;
+    private bool queueClosePlatform;
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-
-        float mod = transform.rotation.eulerAngles.z % 360;
+        queueClosePlatform = false;
+        
         switch (transform.rotation.eulerAngles.z % 360)
         {
             case 0:
@@ -46,28 +47,53 @@ public class Dropper : MonoBehaviour
     {
         if (collision.gameObject.name == "Player")
         {
-            Vector3 lastPlayerSpeed = collision.gameObject.GetComponent<PlayerMovement>().lastSpeed;
-            if (orientation == Orientation.Up && lastPlayerSpeed.y < -forceVelocity
-                || orientation == Orientation.Down && lastPlayerSpeed.y > forceVelocity
-                || orientation == Orientation.Left && lastPlayerSpeed.x > forceVelocity
-                || orientation == Orientation.Right && lastPlayerSpeed.x < -forceVelocity)
+            if (queueClosePlatform)
             {
-                collision.gameObject.GetComponent<Rigidbody2D>().velocity = lastPlayerSpeed;
-                OpenPlatform();
+                queueClosePlatform = false;
+                CancelInvoke("ClosePlatform");
                 Invoke("ClosePlatform", resetTime);
             }
             else
             {
-                Invoke("OpenPlatform", timeUntilDrop);
-                Invoke("ClosePlatform", timeUntilDrop + resetTime);
+                Vector3 lastPlayerSpeed = collision.gameObject.GetComponent<PlayerMovement>().lastSpeed;
+                if (orientation == Orientation.Up && lastPlayerSpeed.y < -forceVelocity
+                    || orientation == Orientation.Down && lastPlayerSpeed.y > forceVelocity
+                    || orientation == Orientation.Left && lastPlayerSpeed.x > forceVelocity
+                    || orientation == Orientation.Right && lastPlayerSpeed.x < -forceVelocity)
+                {
+                    collision.gameObject.GetComponent<Rigidbody2D>().velocity = lastPlayerSpeed;
+                    OpenPlatform();
+                    Invoke("ClosePlatform", resetTime);
+                }
+                else
+                {
+                    Invoke("OpenPlatform", timeUntilDrop);
+                    Invoke("ClosePlatform", timeUntilDrop + resetTime);
+                }
             }
         }
     }
-    
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            if (queueClosePlatform)
+            {
+                ClosePlatform();
+            }
+            else
+            {
+                queueClosePlatform = true;
+            }
+        }
+    }
+
     private void OpenPlatform()
     {
         this.gameObject.layer = LayerMask.NameToLayer("Default");
         hitBox.enabled = false;
+        
         Color color = spriteRenderer.color;
         color.a = 0.5f;
         spriteRenderer.color = color;
@@ -75,10 +101,20 @@ public class Dropper : MonoBehaviour
 
     private void ClosePlatform()
     {
-        this.gameObject.layer = LayerMask.NameToLayer("Ground");
-        hitBox.enabled = true;
-        Color color = spriteRenderer.color;
-        color.a = 1f;
-        spriteRenderer.color = color;
+        if (queueClosePlatform)
+        {
+            this.gameObject.layer = LayerMask.NameToLayer("Ground");
+            hitBox.enabled = true;
+            
+            Color color = spriteRenderer.color;
+            color.a = 1f;
+            spriteRenderer.color = color;
+
+            queueClosePlatform = false;
+        }
+        else
+        {
+            queueClosePlatform = true;
+        }
     }
 }
